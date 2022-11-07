ac7148de859a79114f469f3b1a21f5418c0bb6d8
bskracic <borna.skracic7@gmail.com>
2022-11-07 15:09:49 +0100

    implemented working logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 #include <ctime>
 #include <iostream>
 #include <optional>
+#include <string>
 #include <vector>
 
-const float FPS_LIMIT = 25.0;
-const int WINDOW_HEIGHT = 900;
-const int WINDOW_WIDTH = 1600;
+const int WINDOW_HEIGHT = 700;
+const int WINDOW_WIDTH = 700;
 const int BODY_CHUNK_HEIGHT = 25;
 const int BODY_CHUNK_WIDTH = 25;
 const int FRUIT_RADIUS = 10;
@@ -19,28 +19,27 @@ const sf::Vector2f
     RIGHT = sf::Vector2f(1, 0),
     LEFT = sf::Vector2f(-1, 0);
 
-
 int r = 255;
 
 sf::RectangleShape createChunk()
 {
     sf::RectangleShape chunk(sf::Vector2f(BODY_CHUNK_WIDTH, BODY_CHUNK_HEIGHT));
-    chunk.setFillColor(sf::Color(r, 255, 255));
+    chunk.setFillColor(sf::Color(0, 255, 0));
     r -= 10;
     return chunk;
 }
 
 sf::CircleShape createFruit()
 {
-    float x = rand() % (WINDOW_WIDTH - FRUIT_RADIUS) + FRUIT_RADIUS;
-    float y = rand() % (WINDOW_HEIGHT - FRUIT_RADIUS) + FRUIT_RADIUS;
+    float x = rand() % (WINDOW_WIDTH - FRUIT_RADIUS - 10) + FRUIT_RADIUS + 10;
+    float y = rand() % (WINDOW_HEIGHT - FRUIT_RADIUS - 10) +  FRUIT_RADIUS + 10;
     sf::CircleShape fruit(FRUIT_RADIUS);
-    fruit.setFillColor(sf::Color(0, 255, 0));
+    fruit.setFillColor(sf::Color(200, 200, 200));
     fruit.setPosition(sf::Vector2f(x, y));
     return fruit;
 }
 
-bool assertInsideBounds(sf::Vector2f &movement)
+bool assertInsideBounds(sf::Vector2f movement)
 {
     return !(movement.x < 0 || movement.x > WINDOW_WIDTH - BODY_CHUNK_WIDTH ||
              movement.y < 0 || movement.y > WINDOW_HEIGHT - BODY_CHUNK_HEIGHT);
@@ -54,17 +53,28 @@ int main()
     sf::Vector2f direction = RIGHT, prevDirection;
 
     sf::RectangleShape head(sf::Vector2f(BODY_CHUNK_WIDTH, BODY_CHUNK_HEIGHT));
-    head.setFillColor(sf::Color(255, 255, 255));
+    head.setFillColor(sf::Color(0, 255, 0));
     head.setPosition(sf::Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
     std::vector<sf::RectangleShape> body({head});
     sf::CircleShape fruit = createFruit();
 
+    sf::Text pointsText;
+    pointsText.setPosition(15, 5);
+    pointsText.setCharacterSize(50);
+    pointsText.setFillColor(sf::Color::White);
+    sf::Font arial;
+    if(!arial.loadFromFile("dDicapslock.ttf")){
+        exit(-1);
+    }
+
+    pointsText.setFont(arial);
+
+    float fpsLimit = 15.0;
+    
+    
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Snake");
-    window.setFramerateLimit(FPS_LIMIT);
-
-    long long frame = 0;
-
-    bool directionChanged = false;
+    window.setFramerateLimit(fpsLimit);
+    
     while (window.isOpen())
     {
 
@@ -77,7 +87,6 @@ int main()
 
             if (event.type == sf::Event::KeyPressed)
             {
-                directionChanged = true;
                 switch (event.key.code)
                 {
                 case sf::Keyboard::W:
@@ -98,9 +107,29 @@ int main()
 
         window.clear();
         window.draw(fruit);
-        std::for_each(body.begin(), body.end(), [&window](sf::RectangleShape &chunk){
+
+        for (int i = 0; i < body.size(); i++)
+        {
+            auto chunk = body[i];
+            if (!assertInsideBounds(chunk.getPosition()))
+            {
+                body.clear();
+                sf::RectangleShape head(sf::Vector2f(BODY_CHUNK_WIDTH, BODY_CHUNK_HEIGHT));
+                head.setFillColor(sf::Color(255, 255, 255));
+                head.setPosition(sf::Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
+                body.push_back(head);
+                fpsLimit = 15.0;
+                window.setFramerateLimit(fpsLimit);
+                break;
+            }
             window.draw(chunk);
-        });
+        }
+        pointsText.setString(std::to_string(body.size()-1));
+        window.draw(pointsText);
+        
+        // std::for_each(body.begin(), body.end(), [&window, &body](sf::RectangleShape &chunk){
+        //     window.draw(chunk);
+        // });
         window.display();
 
         std::optional<sf::Vector2f> prevPos;
@@ -126,6 +155,8 @@ int main()
             chunk.setPosition(pos);
             body.push_back(chunk);
             fruit = createFruit();
+            fpsLimit += 0.5;
+            window.setFramerateLimit(fpsLimit);
         }
 
     }
